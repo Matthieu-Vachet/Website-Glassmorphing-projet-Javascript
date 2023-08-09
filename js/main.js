cdc22f74159a77a088884c734586314e9a995837
Matthieu-V <79032023+Matthieu-Vachet@users.noreply.github.com>
2023-08-10 00:09:11 +0200

    add cloud animation

--- js/main.js
+++ js/main.js
@@ -13,21 +13,19 @@ let bk0 = document.getElementById("bk0");
 let bk1 = document.getElementById("bk1");
 let bk2 = document.getElementById("bk2");
 let bk3 = document.getElementById("bk3");
-let bk4 = document.getElementById("bk4");
+// let bk4 = document.getElementById("bk4");
 let bk5 = document.getElementById("bk5");
 let header = document.querySelector("header");
-let title = document.getElementById("title-parallaxe");
 
 window.addEventListener("scroll", function () {
 
   let value = window.scrollY;
   bk0.style.marginLeft = value * 0.1 + "px";
   bk2.style.top = value * 0.30 + "px";
-  bk3.style.top = value * 0.20 + "px";
-  bk4.style.top = value * 0.50 + "px";
-  bk5.style.top = value * 0.20 + "px";
-  header.style.top = value * 0.6 + "px";
-  title.style.marginRight = value * 3 + "px";
+  bk3.style.top = value * 0.30 + "px";
+  // bk4.style.top = value * 0.50 + "px";
+  bk5.style.top = value * 0.40 + "px";
+  header.style.top = value * 0.30 + "px";
 
 
 })
